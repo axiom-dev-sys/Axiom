9874493157b2f610a419707fd129e35d5b758927
JosephJar <josephjar@users.noreply.github.com>
2026-06-22 22:35:55 +0300

    0.9.3: add entity inspector

---
+++ src/DebugTools/InspectorPanel.cpp
@@ -0,0 +1,149 @@
+#include "Axiom/DebugTools/InspectorPanel.hpp"
+#include "Axiom/Scene/Components/TransformComponent.hpp"
+
+#include <imgui.h>
+
+namespace Axiom {
+
+    void InspectorPanel::render()
+    {
+        if (!visible)
+            return;
+
+        ImGui::Begin("Inspector");
+
+        ImGui::Text("Inspector Panel");
+
+        if (!selectedEntity)
+        {
+            ImGui::Text("No selected entity");
+
+            ImGui::End();
+            return;
+        }
+
+        ImGui::Text("Entity");
+
+        ImGui::Separator();
+
+        ImGui::Text("Name: %s", entityName.c_str());
+        ImGui::Text("ID: %u", entityID);
+
+        ImGui::Text(
+            "Active: %s",
+            selectedEntity->isActive()
+            ? "Yes"
+            : "No"
+        );
+
+        ImGui::Text(
+            "Destroyed: %s",
+            selectedEntity->isDestroyed()
+            ? "Yes"
+            : "No"
+        );
+
+        if (ImGui::CollapsingHeader("Transform"))
+        {
+            auto* transform =
+                selectedEntity->getComponent<TransformComponent>();
+
+            if (transform)
+            {
+                ImGui::DragFloat2(
+                    "Position",
+                    &transform->position.x,
+                    1.0f
+                );
+
+                ImGui::DragFloat2(
+                    "Scale",
+                    &transform->scale.x,
+                    1.0f
+                );
+
+                ImGui::DragFloat(
+                    "Rotation",
+                    &transform->rotation,
+                    1.0f
+                );
+            }
+        }
+
+        if (ImGui::CollapsingHeader("Components"))
+        {
+            ImGui::Text(
+                "SpriteComponent: %s",
+                hasSprite ? "Yes" : "No"
+            );
+
+            ImGui::Text(
+                "VelocityComponent: %s",
+                hasVelocity ? "Yes" : "No"
+            );
+
+            ImGui::Text(
+                "ColliderComponent: %s",
+                hasCollider ? "Yes" : "No"
+            );
+
+            ImGui::Text(
+                "PlayerControllerComponent: %s",
+                hasPlayerController ? "Yes" : "No"
+            );
+
+            ImGui::Text(
+                "PlayerTag: %s",
+                hasPlayerTag ? "Yes" : "No"
+            );
+        }
+
+        ImGui::End();
+    }
+
+    void InspectorPanel::setVisible(bool value)
+    {
+        visible = value;
+    }
+
+    bool InspectorPanel::isVisible() const
+    {
+        return visible;
+    }
+
+    void InspectorPanel::toggle()
+    {
+        visible = !visible;
+    }
+
+    void InspectorPanel::setSelectedEntity(Entity* entity)
+    {
+        selectedEntity = entity;
+    }
+
+    void InspectorPanel::setHasSprite(bool value)
+    {
+        hasSprite = value;
+    }
+
+    void InspectorPanel::setHasVelocity(bool value)
+    {
+        hasVelocity = value;
+    }
+
+    void InspectorPanel::setHasCollider(bool value)
+    {
+        hasCollider = value;
+    }
+
+    void InspectorPanel::setHasPlayerController(bool value)
+    {
+        hasPlayerController = value;
+    }
+
+    void InspectorPanel::setHasPlayerTag(bool value)
+    {
+        hasPlayerTag = value;
+    }
+
+}
