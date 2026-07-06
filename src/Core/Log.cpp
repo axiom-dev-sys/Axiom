916a4a4ed6fd95ff787f02ae808827ad7be8eee3
JosephJar <josephjar@users.noreply.github.com>
2026-07-06 11:08:00 +0300

    Improve Console panel functionality

--- src/Core/Log.cpp
+++ src/Core/Log.cpp
@@ -1,21 +1,46 @@
 #include "Axiom/Core/Log.hpp"
 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
 namespace Axiom {
+
+    static std::string getCurrentTime()
+    {
+        auto now = std::chrono::system_clock::now();
+        std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+        std::tm localTime{};
+
+#ifdef _WIN32
+        localtime_s(&localTime, &time);
+#else
+        localTime = *std::localtime(&time);
+#endif
+
+        std::ostringstream stream;
+
+        stream << std::put_time(&localTime, "%H:%M:%S");
+
+        return stream.str();
+    }
 
     void Log::message(Level level, const std::string& msg)
     {
         switch (level)
         {
         case Level::Info:
-            std::cout << "[INFO] " << msg << std::endl;
+            std::cout << "[" << getCurrentTime() << "] " << "[INFO] "  << msg << std::endl;
             break;
 
         case Level::Warn:
-            std::cout << "[WARN] " << msg << std::endl;
+            std::cout << "[" << getCurrentTime() << "] " << "[WARNING] " << msg << std::endl;
             break;
 
         case Level::Error:
-            std::cerr << "[ERROR] " << msg << std::endl;
+            std::cerr << "[" << getCurrentTime() << "] " << "[ERROR] " << msg << std::endl;
             break;
         }
     }
